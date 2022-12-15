c5a753039d879a10831546b682f0d435aa7b171a
deysebonisegnia <dbonisegniabf@gmail.com>
2022-12-15 19:25:12 -0300

    correcao

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// passo 1 
+// exercicios do checkpoint 3 DH 
    
     class Aluno {
         constructor(nome,faltas,notas){
@@ -7,48 +7,56 @@
         this.notas = notas;
     }
 
-    
-
-
    calcularMedia(){
-        let soma = 0
-        this.notas.forEach(nota => {
-            soma += nota
+        let soma = 0;
+        this.notas.forEach((nota )=> {
+            soma += nota;
         });
-        return soma / this.notas.length
+        return soma / this.notas.length;
     } 
-    faltas(){
-        return this.faltas += 1
+    adicionarFaltas(){
+        return (this.faltas += 1);
     }
     
 }
+const aluno1 = new Aluno('Maria', 1, [3, 4, 5]);
+const aluno2 = new Aluno('José', 2, [7, 5, 5]);
+console.log(aluno1.calcularMedia());
+aluno2.adicionarFaltas();
+console.log(aluno2);
+
    let curso = {
-    nome: 'peterson',
+    nome: 'javascript',
     nota: 5,
     qtmaxfaltas:5,
     listEstud:[],
 
     addAluno(nome,qtmaxfaltas,notas){
-        this.listEstud.push(new Aluno(nome,qtmaxfaltas,notas))
+        this.listEstud.push(new Aluno(nome,qtmaxfaltas,notas));
     },
      mediaFinal(aluno){
-       let media = aluno.medias()
+       let media = aluno.calcularMedia();
         if (media >= this.nota && aluno.faltas < this.qtmaxfaltas){
-            return true
-        }else if(aluno.faltas === this.qtmaxfaltas && media >= this.nota*1.1){
-            return true 
+            return true;
+
+        }else if(aluno.faltas === this.qtmaxfaltas && media >= this.nota * 1.1){
+            return true ;
             
-        }else return false
+        }else return false;
        
 },
    alunosAprovado(){
-    this.listEstud.map((aluno) => this.mediaFinal(aluno))
+    return this.listEstud.map((aluno) => this.mediaFinal(aluno));
    }
    }
    
    curso.addAluno('deyse',2,[3, 4, 5]);
     curso.addAluno('taciano',2,[4, 7, 6]);
-       const aluno1 = curso.addAluno('maria',2,[5, 4, 5])
+     curso.addAluno('maria',2,[5, 4, 5]);
+     curso.addAluno('adilson',2,[7, 8, 5]);
+     curso.addAluno('bianca',2,[5, 4, 5]);
 
-        console.log(curso.listEstud)
-        console.log( curso.mediaFinal(aluno1))
+
+
+        console.log(curso.listEstud);
+        console.log( curso.alunosAprovado());
